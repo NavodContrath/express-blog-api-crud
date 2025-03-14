25c956d15fdbf0e049b15e5ba2792a9a8842d643
NavodContrath <navod.contrath@gmail.com>
2025-03-14 15:50:45 +0100

    add middleware for router

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const postRouter = require('./routers/post_router')
 
 //listening to
 app.listen(port, () => {
     console.log(`server is listening on http://localhost:${port}`)
 })
+//middleware
+app.use('/posts', postRouter)
