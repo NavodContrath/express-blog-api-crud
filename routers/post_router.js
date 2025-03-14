0e105b3be9ff85fe8f11f8e001c931ded15ebab3
NavodContrath <navod.contrath@gmail.com>
2025-03-14 15:49:47 +0100

    add show route

--- routers/post_router.js
+++ routers/post_router.js
@@ -7,3 +7,15 @@ router.get('/', (req, res) => {
     res.json(posts)
 })
 //show
+router.get('/:slug', (req, res) => {
+    //find post by slug
+    const foundPost = posts.find(post => post.slug === req.params.slug)
+    //error handler
+    if (!foundPost) {
+        res.status(404).json({
+            error: "404 not found",
+            message: "post not found"
+        })
+
+    }
+})
