69c9fb3b0e591ed6c82e84801936f124c2ec0f8e
NavodContrath <navod.contrath@gmail.com>
2025-03-28 16:27:38 +0100

    add cors middleware for handling cross-origin requests

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 const postRouter = require('./routers/post_router')
 const serverError = require('./middlewares/errors/serverError')
 const error404 = require('./middlewares/errors/error404')
 
+
 //listening to
 app.listen(port, () => {
     console.log(`server is listening on http://localhost:${port}`)
 })
+//middleware for cors
+app.use(cors({
+    //define who can access it
+    origin: 'http://localhost:5173'
+}))
 //middleware to parse the content in json
 app.use(express.json())
 //middleware to define routes
